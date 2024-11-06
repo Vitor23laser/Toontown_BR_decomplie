097e0f58eb962f977acd64ec822913ed181bbc33
Vitor26laser <97393601+Vitor23laser@users.noreply.github.com>
2024-11-06 12:07:14 -0300

    Add files via upload

--- otp/__init__.py
+++ otp/__init__.py
@@ -1,1 +1,13 @@
+'''
+/*
+*  Generator: vscode-decompiler@0.1.0 (https://marketplace.visualstudio.com/items?itemName=tintinweb.vscode-decompiler)
+*  Target:    c:\Users\vitor.soliveira19\Toontown_BR\otp\__init__.pyc
+**/
+'''
 
+# uncompyle6 version 3.9.2
+# Python bytecode version base 2.4 (62061)
+# Decompiled from: Python 2.7.16 (v2.7.16:413a49145e, Mar  4 2019, 01:37:19) [MSC v.1500 64 bit (AMD64)]
+# Embedded file name: otp.ai
+pass
+;
